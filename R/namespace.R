5722989364a1f0985f144f1fb143c71f4f12fd19
Seb Dalgarno <seb@poissonconsulting.ca>
2024-04-08 16:35:59 -0700

    add dplyr funs to namespace

--- R/namespace.R
+++ R/namespace.R
@@ -1,6 +1,7 @@
 #' @import chk glue nimble
 #' @importFrom scales percent
 #' @importFrom rescale rescale
+#' @importFrom dplyr arrange tibble bind_rows left_join
 #' @importFrom purrr quietly map map_dbl map_lgl
 #' @importFrom stats setNames update median coef predict nobs plogis runif qnorm logLik
 #' @importFrom ggplot2 autoplot .data ggplot aes geom_pointrange geom_hline scale_x_continuous scale_x_discrete scale_y_continuous expand_limits xlab geom_line geom_ribbon
