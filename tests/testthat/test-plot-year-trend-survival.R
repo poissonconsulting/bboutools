feafd243b57ce537e575a68be8e9b06e54d0b9fa
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-17 14:58:05 -0700

    move trend plot tests into separate scripts

---
+++ tests/testthat/test-plot-year-trend-survival.R
@@ -0,0 +1,40 @@
+# Copyright 2022-2023 Integrated Ecological Research and Poisson Consulting Ltd.
+# Copyright 2024 Province of Alberta
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+test_that("bb_plot_year_trend_survival.bboufit_survival works", {
+  plot <- bb_plot_year_trend_survival(bboutools:::fit_survival_trend)
+  expect_s3_class(plot, "ggplot")
+  expect_snapshot_plot(plot, "plot_year_trend_survival")
+})
+
+test_that("bb_plot_year_trend_survival.data.frame works", {
+  prediction <- bb_predict_survival_trend(bboutools:::fit_survival_trend)
+  plot <- bb_plot_year_trend_survival(prediction)
+  expect_s3_class(plot, "ggplot")
+  expect_snapshot_plot(plot, "plot_year_trend_survival_df")
+})
+
+test_that("bb_plot_year_trend_survival.bboufit_survival ML works", {
+  plot <- bb_plot_year_trend_survival(bboutools:::fit_survival_ml_trend)
+  expect_s3_class(plot, "ggplot")
+  expect_snapshot_plot(plot, "plot_year_trend_survival_ml")
+})
+
+test_that("bb_plot_year_trend_survival.data.frame ML works", {
+  prediction <- bb_predict_survival_trend(bboutools:::fit_survival_ml_trend)
+  plot <- bb_plot_year_trend_survival(prediction)
+  expect_s3_class(plot, "ggplot")
+  expect_snapshot_plot(plot, "plot_year_trend_survival_ml_df")
+})
